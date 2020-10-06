8d90684cf95bd1ee3ff37ec76ca91a1f18aaf13b
Ezeche L. Samuel <62204958+LanreSam@users.noreply.github.com>
2020-10-06 10:30:51 -0500

    11th project

---
+++ 11-tabs/app.js
@@ -0,0 +1,23 @@
+const about = document.querySelector('.about');
+const btns = document.querySelectorAll('.tab-btn');
+const articles = document.querySelectorAll('.content');
+
+
+about.addEventListener('click', function (e) {
+    console.log(e.target.dataset.id);
+    const id = e.target.dataset.id;
+    if (id) {
+        //remove active from other buttons
+        btns.forEach(function (btn) {
+            btn.classList.remove('active');
+            e.target.classList.add('active');
+        });
+
+        //hide other articles
+        articles.forEach(function (article) {
+            article.classList.remove('active')
+        });
+        const element =document.getElementById(id);
+        element.classList.add('active');
+    }
+});
