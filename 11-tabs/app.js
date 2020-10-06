3ca072ee87c97f757b4d44816ee7f8952a1e9f54
Ezeche L. Samuel <62204958+LanreSam@users.noreply.github.com>
2020-10-06 10:52:33 -0500

    made a correction

--- 11-tabs/app.js
+++ 11-tabs/app.js
@@ -7,7 +7,7 @@ variable declarations
 const about = document.querySelector('.about');
 const btns = document.querySelectorAll('.tab-btn');
 const articles = document.querySelectorAll('.content');
-const date = document.querySelector("date");
+const date = document.querySelector(".date");
 
 
 about.addEventListener('click', function (e) {
